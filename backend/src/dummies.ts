c5d42fbf805e907a420e8123f12d90fa3f511008
Patricio Garcia <patricio.garciaort28@gmail.com>
2026-07-27 10:06:24 -0600

    Add: Datos dummy
    Se crearon los datos dummy siguiendo el tipado que genero prisma

---
+++ backend/src/dummies.ts
@@ -0,0 +1,76 @@
+import { Usuario, Rol, Departamento, Entidad, Politica,Archivo,VersionArchivo } from "./generated/prisma";
+
+export const rolesDummy: Rol[] =[
+    { id: 1, rol: 'Admin', descripcion: 'Administrador del sistema' },
+    { id: 2, rol: 'Auditor', descripcion: 'Solo lectura y revisión' },
+];
+
+export const entidadesDummy: Entidad[] = [
+  {
+    id: 1,
+    planta: 'Planta Monterrey',
+    pais: 'México',
+    estado: 'Nuevo León',
+    direccion: 'Av. Industrial 123',
+  },
+];
+
+export const departamentosDummy: Departamento[] = [
+  { id: 1, nombre: 'Recursos Humanos', entidadId: 1 },
+  { id: 2, nombre: 'IT', entidadId: 1 },
+];
+
+export const usuariosDummy: Usuario[] = [
+  {
+    id: 1,
+    correo: 'admin@empresa.com',
+    contraseña: 'hashed_password_123',
+    usuarioActiveDirectory: 'CORP\\admin_user',
+  },
+  {
+    id: 2,
+    correo: 'juan.perez@empresa.com',
+    contraseña: 'hashed_password_456',
+    usuarioActiveDirectory: 'CORP\\jperez',
+  },
+];
+
+
+export const politicasDummy: Politica[] = [
+  {
+    id: 1,
+    titulo: 'Política de Seguridad de la Información',
+    departamentoId: 2, // Sistemas / IT
+    subidoPor: 1,      // Admin
+    fechaSubida: new Date(),
+    modificadoPor: 1,
+    fechaModificado: new Date(),
+  },
+];
+
+
+export const archivosDummy: Archivo[] = [
+  {
+    id: 1,
+    codigo: 'POL-IT-001',
+    politicaId: 1,
+    subidoPor: 1,
+    fechaSubida: new Date(),
+    modificadoPor: 1,
+    fechaModificado: new Date(),
+  },
+];
+
+export const versionesDummy: VersionArchivo[] = [
+  {
+    id: 1,
+    archivoId: 1,
+    ruta: '/uploads/politicas/POL-IT-001_v1.pdf',
+    version: 1,
+    active: true,
+    subidoPor: 1,
+    fechaSubida: new Date(),
+    modificadoPor: 1,
+    fechaModificado: new Date(),
+  },
+];

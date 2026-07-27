43705189c76ad891405b808c51d1e325214fc128
Patricio Garcia <patricio.garciaort28@gmail.com>
2026-07-27 11:40:17 -0600

    Add: Configuracion de router
    Se configuro el index para que use las direcciones de nuestro router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
-
+// 1. Importamos el archivo de rutas (asegúrate de que el nombre del archivo coincida con el tuyo)
+import routes from './routes';
 const app = express();
 const puerto = 4000;
 
-// Tu primera ruta (Endpoint)
+// 2. ¡MUY IMPORTANTE! Esto permite que Express entienda el `req.body` en formato JSON
+app.use(express.json());
+
+// Tu ruta de prueba original
 app.get('/', (req: Request, res: Response) => {
   res.json({ mensaje: "¡Hola! Mi servidor Express con TypeScript funciona perfectamente." });
 });
+
+// 3. Registramos tus rutas de autenticación bajo el prefijo '/api/auth'
+app.use('/api/auth', routes);
 
 app.listen(puerto, () => {
   console.log(`🚀 Servidor encendido y escuchando en http://localhost:${puerto}`);

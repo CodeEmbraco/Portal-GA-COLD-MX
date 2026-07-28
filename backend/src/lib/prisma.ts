6cff9daa27ea665b74d57bbd7805251ca49e11c6
Patricio Garcia <patricio.garciaort28@gmail.com>
2026-07-28 12:13:51 -0600

    Add: Separacion de instancia de prisma
    Ahora solo se debera de importar desde aqui para evitar instanciarlo en cada archivo

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '../generated/prisma'; 
+import { PrismaMssql } from '@prisma/adapter-mssql';
+
+const connectionString = process.env.DATABASE_URL as string;
+const adapter = new PrismaMssql({ connectionString });
+
+// Exportamos UNA SOLA instancia para toda la app
+export const prisma = new PrismaClient({ adapter });

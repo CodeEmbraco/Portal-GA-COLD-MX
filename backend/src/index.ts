1defafa8b41404f64c3402f0bedf531a4b1c871b
Patricio Garcia <patricio.garciaort28@gmail.com>
2026-07-28 14:19:07 -0600

    Add: Header en CORS
    Se agrega otro header al cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization','x-user-data']
 }));
 
 //Esto permite que Express entienda el `req.body` en formato JSON

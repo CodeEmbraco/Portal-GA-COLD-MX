df78bb7060711039405794483556fde55f23c449
Patricio Garcia Ortega <patricio.o.garcia@acim.nidec.com>
2026-07-30 13:08:53 -0600

    Add: Separacion de back

--- backend/src/routes/catalogRouter.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { CatalogoController } from '../controllers/catalogoController';
-
-const catalogoRouter = Router();
-catalogoRouter.get('/:catalogo', CatalogoController.getCatalog);
-
-export default catalogoRouter;

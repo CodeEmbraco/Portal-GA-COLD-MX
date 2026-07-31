b450fad8b5f09e2b822bac2f67b45a11e9384392
CodeEmbraco <codenidecembraco@gmail.com>
2026-07-31 07:57:37 -0600

    add: ajustes de despliegue

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || ['http://localhost:8009','http://10.13.225.19:8009'],
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
